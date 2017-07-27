88951133a67a715dea227d5acb0162745385871e
tjadanel <mikiozen@gmail.com>
2017-07-27 17:20:45 -0400

    ch3 examples

---
+++ C_Programming_A_Modern_Approach/ch3/tprintf.c
@@ -0,0 +1,13 @@
+// prints int and float values in various formats
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 40;
+    float x = 839.21f;
+
+    printf("|%d|%5d|%-5d|%5.3d|\n", i, i, i, i);
+    printf("|%10.3f|%10.3e|%-10g|\n", x, x, x);
+
+    return 0;
+}
